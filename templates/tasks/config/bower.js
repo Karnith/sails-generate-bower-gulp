8b9011034d81e3be225c2626586ade1fe144fe94
Matthew Marino <mmarino@jcmar.com>
2014-08-12 15:59:24 -0700

    Update bower.js

--- templates/tasks/config/bower.js
+++ templates/tasks/config/bower.js
@@ -8,14 +8,14 @@
  */
 
 module.exports = function(gulp, plugins) {
-	gulp.task('bower', function() {
-		var install = plugins.bower()
+	gulp.task('bower:install', function() {
+		return plugins.bower()
 				.pipe(gulp.dest('./bower_components'))
 				.pipe(plugins.notify({ message: 'Bower install task complete' }));
-	
-		var copy = gulp.src('./bower_components/**/*.min.*')
+	});
+	gulp.task('bower:copy', function() {
+		return gulp.src('./bower_components/**/*.min.*')
 				.pipe(gulp.dest('./assets/vendor'))
 				.pipe(plugins.notify({ message: 'Bower copy task complete' }));
-		return plugins.stream(install, copy);
 	});
 };
