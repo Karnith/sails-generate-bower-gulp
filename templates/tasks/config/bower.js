dbb257d9c9e65108d2b073fc353f1a667d33ff4f
Matthew Marino <mmarino@jcmar.com>
2014-08-14 10:17:36 -0700

    changed task to use gulp-bower-files

--- templates/tasks/config/bower.js
+++ templates/tasks/config/bower.js
@@ -14,7 +14,7 @@ module.exports = function(gulp, plugins, growl) {
 				.pipe(plugins.if(growl, plugins.notify({ message: 'Bower install task complete' })));
 	});
 	gulp.task('bower:copy', function() {
-		return gulp.src('./bower_components/**/*.min.*')
+		return plugins.files()
 				.pipe(gulp.dest('./assets/vendor'))
 				.pipe(plugins.if(growl, plugins.notify({ message: 'Bower copy task complete' })));
 	});
